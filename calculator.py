cf9b4373d49d0b3fad78d3d49321b7a51766166f
Tirth Parmar <163869222+Tirthp1507@users.noreply.github.com>
2024-05-24 19:06:28 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+A = int(input("Enter number 1"))
+B = int(input("Enter number 2"))
+
+Addition = A + B
+print("Addition of A and B is", Addition )
+Subtraction = A - B
+print("Subtraction of A and B is", Subtraction )
+Division = A/B
+print("Div of A and B is",Division )
+Multiplication = A*B
+print("Mul of A and B is", Multiplication )
+
+nm ="Harry"
+print(nm[-4:-2])
+
+a = int(input("Enter your age :"))
+print(a)
+
+if (a>18):
+    print("Yes, you can drive")
+elif(a==18):
+    print("You can apply for license")
+else:
+    print("No, you can't drive")
